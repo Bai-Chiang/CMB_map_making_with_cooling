efd825a4e1de034e20930f0a983aed6f25815c2e
Bai-Qiang <qiang.baiqiang@gmail.com>
2021-06-14 12:03:06 -0400

    rewrite Future Prospects

--- plot_paper_images.py
+++ plot_paper_images.py
@@ -104,9 +104,9 @@ for data_dic in data_list:
             (chi2-chi2_min)/(chi2[0]-chi2_min),
             label='CG'
         )
-    with open (data_dic['CG_PT_auto_eta_file'], 'rb') as _file:
-        CG_PT_result = pickle.load(_file)
-        chi2 = CG_PT_result['chi2_hist']
+    with open (data_dic['CG_eta_file'], 'rb') as _file:
+        CG_eta_result = pickle.load(_file)
+        chi2 = CG_eta_result['chi2_hist']
         axs[fig_index].plot(
             (chi2-chi2_min)/(chi2[0]-chi2_min),
             label='CG with $\eta$'
@@ -151,9 +151,9 @@ for data_dic in data_list:
             (chi2-chi2_min)/(chi2[0]-chi2_min),
             label='CG'
         )
-    with open (data_dic['CG_PT_auto_eta_file'], 'rb') as _file:
-        CG_PT_result = pickle.load(_file)
-        chi2 = CG_PT_result['chi2_hist']
+    with open (data_dic['CG_eta_file'], 'rb') as _file:
+        CG_eta_result = pickle.load(_file)
+        chi2 = CG_eta_result['chi2_hist']
         axs[fig_index].plot(
             (chi2-chi2_min)/(chi2[0]-chi2_min),
             label='CG with $\eta$'
@@ -197,18 +197,18 @@ for data_dic in data_list:
             (chi2-chi2_min)/(chi2[0]-chi2_min),
             label='CG'
         )
-    with open (data_dic['CG_PT_auto_eta_file'], 'rb') as _file:
-        CG_PT_result = pickle.load(_file)
-        chi2 = CG_PT_result['chi2_hist']
+    with open (data_dic['CG_eta_file'], 'rb') as _file:
+        CG_eta_result = pickle.load(_file)
+        chi2 = CG_eta_result['chi2_hist']
         axs[fig_index].plot(
             (chi2-chi2_min)/(chi2[0]-chi2_min),
             label='CG with $\eta$'
         )
     for num_eta in num_eta_arr:
-        with open (data_dic['CG_PT_manual_ln_{:d}_eta_file'.format(num_eta)],
+        with open (data_dic['CG_manual_ln_{:d}_eta_file'.format(num_eta)],
                 'rb') as _file:
-            CG_PT_result = pickle.load(_file)
-            chi2 = CG_PT_result['chi2_hist']
+            CG_eta_result = pickle.load(_file)
+            chi2 = CG_eta_result['chi2_hist']
             axs[fig_index].plot(
                 (chi2-chi2_min)/(chi2[0]-chi2_min),
                 label='CG with $n_{{\eta}} = {:d}$'.format(num_eta)
@@ -223,4 +223,66 @@ for ax in axs:
     ax.label_outer()
 #plt.show()
 plt.savefig(figs_dir/'chi2_neta.pdf', bbox_inches="tight")
+plt.close()
+
+# plot exact eta results
+fig = plt.figure(figsize=(9,3))
+gs = fig.add_gridspec(1,3, wspace=0)
+axs = gs.subplots(sharey='row')
+for data_dic in data_list:
+    f_scan = data_dic['f_scan']
+    f_knee = data_dic['f_knee']
+    f_apo = data_dic['f_apo']
+    chi2_min = data_dic['chi2_min']
+    if f_knee == 10 and f_apo == 0:
+        fig_index = 0
+        axs[0].set_title('$f_{knee}=10$\n$f_{apo}=0$')
+    elif f_knee == 1 and f_apo == 0:
+        fig_index = 1
+        axs[1].set_title('$f_{knee}=1$\n$f_{apo}=0$')
+    elif f_knee == 0.1 and f_apo == 0.001:
+        fig_index = 2
+        axs[2].set_title('$f_{knee}=0.1$\n$f_{apo}=0.001$')
+    else:
+        continue
+    with open (data_dic['CG_SP_file'], 'rb') as _file:
+        CG_SP_result = pickle.load(_file)
+        chi2 = CG_SP_result['chi2_hist']
+        axs[fig_index].plot(
+            (chi2-chi2_min)/(chi2[0]-chi2_min),
+            label='CG'
+        )
+    with open (data_dic['CG_eta_file'], 'rb') as _file:
+        CG_eta_result = pickle.load(_file)
+        chi2 = CG_eta_result['chi2_hist']
+        axs[fig_index].plot(
+            (chi2-chi2_min)/(chi2[0]-chi2_min),
+            label='CG with $\eta$'
+        )
+    with open (data_dic['CG_exact_eta_file'], 'rb') as _file:
+        CG_exact_eta_result = pickle.load(_file)
+        chi2 = CG_exact_eta_result['chi2_hist']
+        axs[fig_index].plot(
+            (chi2-chi2_min)/(chi2[0]-chi2_min),
+            label=r'CG with $\delta\eta = \frac{\chi^2}{-\frac{d}{d\eta}\chi^2}$'
+        )
+    #for num_eta in num_eta_arr:
+    #    with open (data_dic['CG_manual_ln_{:d}_eta_file'.format(num_eta)],
+    #            'rb') as _file:
+    #        CG_eta_result = pickle.load(_file)
+    #        chi2 = CG_eta_result['chi2_hist']
+    #        axs[fig_index].plot(
+    #            (chi2-chi2_min)/(chi2[0]-chi2_min),
+    #            label='CG with $n_{{\eta}} = {:d}$'.format(num_eta)
+    #        )
+    axs[fig_index].set_yscale('log')
+    axs[fig_index].set_ylim(1e-12,1)
+    axs[fig_index].grid()
+axs[0].set_ylabel(r'$\frac{\chi^2-\chi^2_{min}} {\chi^2_{ini}- \chi^2_{min}}$') 
+axs[1].set_xlabel('number of iteration')
+axs[2].legend(bbox_to_anchor=(1,1), loc="upper left")
+for ax in axs:
+    ax.label_outer()
+#plt.show()
+plt.savefig(figs_dir/'chi2_exact_eta.pdf', bbox_inches="tight")
 plt.close()
